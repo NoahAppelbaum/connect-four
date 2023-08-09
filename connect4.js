a4f8f9966aa14a3f06bd29c9969fbbcc6cc7f705
Megan Vyenielo <mvyenielo58@gmail.com>
2023-08-09 15:11:35 -0700

    created makeBoard function

--- connect4.js
+++ connect4.js
@@ -7,18 +7,21 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+const BOARD = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  //  set "board" to empty HEIGHT x WIDTH matrix array
+  for (let row = 0; row < HEIGHT; row++) {
+    BOARD.push(Array(WIDTH).fill(null));
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
@@ -29,7 +32,7 @@ function makeHtmlBoard() {
   // TODO: add comment for this code
   var top = document.createElement("tr");
   top.setAttribute("id", "column-top");
-  
+
   // TODO: add comment for this code
   for (var x = 0; x < WIDTH; x++) {
     var headCell = document.createElement("td");
